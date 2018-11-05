cdcef9de3d3fd865af643d3689ba658aa6e74919
Giuseppe Bianco <giuseppe.bianco@accenture.com>
2018-11-05 11:39:24 +0100

    second commit

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -19,7 +19,7 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 		
 		RestTemplate rt = new RestTemplate();
-		rt.getForObject("http://localhost:8080/prova", Void.class);
+		rt.getForObject("http://mediaset-api.herokuapp.com/prova", Void.class);
 	}
 	
 	
